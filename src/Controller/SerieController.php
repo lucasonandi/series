c644644d7ebd5f56782be94a8e79540b86a4d719
lonandi2022 <lucas.onandi2022@campus-eni.fr>
2023-02-22 15:30:29 +0100

    recuperation des seriies plus populares et du detail d'une serie

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Serie;
+use App\Repository\SerieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,23 +13,66 @@ use Symfony\Component\Routing\Annotation\Route;
 class SerieController extends AbstractController
 {
     #[Route('/list', name: 'list')]
-    public function index(): Response
+    public function list(SerieRepository $serieRepository): Response
     {
 
         //TODO recuperer la liste des series en BDD
-        return $this->render('serie/list.html.twig');
+        //on recupere toutes les series en passant par le repository
+       // $series = $serieRepository->findAll();
+
+        //$series = $serieRepository->findBy(["status" => "ended"], ["popularity"=>'DESC'], 10);
+
+        $series = $serieRepository->findBy([],["vote"=>"DESC"], 50);
+
+        dump($series);
+        return $this->render('serie/list.html.twig', [
+            // on envoie les donnees a la vue
+            'series'=>$series
+        ]);
     }
     #[Route('/{id}', name: 'show', requirements: ['id' => '\d+'])]
-    public function show(int $id): Response
+    public function show(int $id, SerieRepository $serieRepository): Response
     {
+        $serie = $serieRepository->find($id);
         dump($id);
 
         //TODO recupereration des infos de la serie en BDD
-        return $this->render('serie/show.html.twig');
+        return $this->render('serie/show.html.twig', ['serie' =>$serie]);
     }
     #[Route('/add', name: 'add')]
-    public function add(): Response
+    public function add(SerieRepository $serieRepository, EntityManagerInterface $entityManager): Response
     {
+
+        $serie = new Serie();
+
+        $serie
+            ->setName("Le magicien")
+            ->setBackdrop("backdrop.png")
+            ->setDateCreated(new \DateTime())
+            ->setGenres("Comedy")
+            ->setFirstAirDate(new \DateTime('2022-02-02'))
+            ->setLastAirDate(new \DateTime("-6 month"))
+            ->setPopularity(850.52)
+            ->setPoster("poster.png")
+            ->setTmdbId(123456)
+            ->setVote(8.5)
+            ->setStatus("Ended");
+
+        $entityManager->persist($serie);
+        $entityManager->flush();
+//        dump($serie);
+//
+//        $serieRepository->save($serie, true);
+//
+//        dump($serie);
+//        $serie->setName("The last of us");
+//        $serieRepository->save($serie, true);
+//
+//        dump($serie);
+
+        $serieRepository->remove($serie, true);
+        dump($serie);
+
 
         //TODO recuperer la liste des series en BDD
         return $this->render('serie/add.html.twig');
